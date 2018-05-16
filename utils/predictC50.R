42bf8020c889b5e2693fde1cdff81687e412740b
Pedro del Pozo <pedrodpj92@gmail.com>
2018-05-16 16:33:54 +0200

    minor changes

--- utils/predictC50.R
+++ utils/predictC50.R
@@ -23,6 +23,9 @@ option_list <- list(
   make_option(c("-o","--outputData", type="character", default = NULL),
               help="path to indicate where predictions should be saved.",
               metavar="character"),
+  make_option(c("-n","--nameFiles", type="character", default = NULL),
+              help="name for identify output files. There will be two files, a csv file with all possible predictions (with binary decisions) and a ttl selecting types from csv file with positive binary decisions. Extensions ('.csv' and '.ttl') will be added automatically, so do not include that in -n option",
+              metavar="character"),
   make_option(c("-d","--domain", type="character", default = NULL),
               help="indicates if it is English or Spanish. It means, if URIs starts with '<http://dbpedia.org/resource/' or '<http://es.dbpedia.org/resource/'.",
               metavar="character")
@@ -53,11 +56,11 @@ if(opt$domain %in% c("ES")){
 }
 
 prepare_properties(file_properties_In = opt$inputData, 
-                   file_object_propertiesMatrix_Out = opt$outputData,
+                   file_object_propertiesMatrix_Out = paste(opt$outputData,"objects_properties_Matrix.csv",sep=''),
                    domain_resourcesURI = dominio)
 
 
-df_data <- read.csv(file=paste(opt$outputData,"objects_properties_Matrix.csv",sep = ''),
+df_data <- read.csv(file=paste(opt$outputData,"objects_properties_Matrix.csv",sep=''),
                         header=FALSE, sep=",", encoding = "UTF-8", stringsAsFactors = FALSE)
 colnames(df_data) <- df_data[1,]
 df_data <- df_data[-1,]
@@ -67,18 +70,27 @@ df_data[,c(2:(ncol(df_data)))] <- sapply(df_data[,c(2:(ncol(df_data)))], as.nume
 load(file = opt$modelPath)
 
 c50_nivel1_v3 <- lista_modelos[1]
+c50_nivel1_v3 <- c50_nivel1_v3[[1]]
 
 c50_n2_m1 <- lista_modelos[2]
+c50_n2_m1 <- c50_n2_m1[[1]]
 c50_n2_m3 <- lista_modelos[3]
+c50_n2_m3 <- c50_n2_m3[[1]]
 
 c50_n3_m1 <- lista_modelos[4]
+c50_n3_m1 <- c50_n3_m1[[1]]
 c50_n3_m3 <- lista_modelos[5]
+c50_n3_m3 <- c50_n3_m3[[1]]
 
 c50_n4_m1 <- lista_modelos[6]
+c50_n4_m1 <- c50_n4_m1[[1]]
 c50_n4_m3 <- lista_modelos[7]
+c50_n4_m3 <- c50_n4_m3[[1]]
 
 c50_n5_m1 <- lista_modelos[8]
+c50_n5_m1 <- c50_n5_m1[[1]]
 c50_n5_m3 <- lista_modelos[9]
+c50_n5_m3 <- c50_n5_m3[[1]]
 
 
 
@@ -94,7 +106,7 @@ test1_n5_m1 <- predict(object = c50_n5_m1, newdata = df_data[,c(2:(ncol(df_data)
 test1_n5_m3 <- predict(object = c50_n5_m3, newdata = df_data[,c(2:(ncol(df_data)))])
 
 
-est1 <- cbind(as.data.frame(df_data[,1]),     #1
+test1 <- cbind(as.data.frame(df_data[,1]),     #1
               test1_n1,    #2
               test1_n2_m1, #3
               test1_n2_m3, #4
@@ -115,7 +127,7 @@ colnames(test1) <- c("s",          #1
                      "Class5_m1",  #9
                      "Class5_m3")  #10
 
-write.csv(test1, file = paste(pathOutput,nameOutputFile,".csv",sep=""), fileEncoding = "UTF-8", row.names=FALSE)
+write.csv(test1, file = paste(opt$outputData,opt$nameFiles,".csv",sep=''), fileEncoding = "UTF-8", row.names=FALSE)
 
 salida_test1_n1 <- test1[,c(1,2)]
 colnames(salida_test1_n1) <- c("s","o")
@@ -136,7 +148,7 @@ salida_test1 <- rbind(salida_test1_n1,
 
 salida_test1$p <- "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
 salida_test1[,c(1,2,3)] <- salida_test1[,c(1,3,2)]
-write.table(salida_test1, file = paste(pathOutput,nameOutputFile,".ttl",sep=""),
+write.table(salida_test1, file = paste(opt$outputData,opt$nameFiles,".ttl",sep=''),
             fileEncoding = "UTF-8", sep = " ", row.names=FALSE, col.names=FALSE, quote = FALSE)
 
 
